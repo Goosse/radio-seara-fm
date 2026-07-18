afda2c17515373f24ec42105f40341debda4ddc4
Goosse <jigoossen@gmail.com>
2026-07-18 14:52:40 +0000

    Create Minha História “nycolle-ramos”

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //Somehow I need to get a build tag built into thise serviceworker file, because caches will only be updated if this file changes.
 
-const buildTag = "2026718125748";
+const buildTag = "202671814525";
 const precachedResources = ["/", "/algemas-quebradas/", "/recursos/capas/semmarca/algemas-quebradas.webp", "/culto-ice-nova-russas/", "/recursos/capas/semmarca/culto-ice-nova-russas.webp", "/familia-bencao-de-deus/", "/recursos/capas/semmarca/familia-bencao-de-deus.webp", "/jornal-seara/", "/recursos/capas/semmarca/jornal-seara.webp", "/ligado-na-verdade/", "/recursos/capas/semmarca/ligado-na-verdade.webp", "/minha-historia/", "/recursos/capas/semmarca/minha-historia.webp", "/seara-esporte-clube/", "/recursos/capas/semmarca/seara-esporte-clube.webp", "/baixar-algemas-quebradas/", "/a-esposa-por-tras/", "/baixar-minha-historia/", "/parceiros/", "/styles.css"];
 const origin = new URL(location).origin;
 
