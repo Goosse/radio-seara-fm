d21bb730cd623bb05b761d3b2916cf18535ab85c
Goosse <jigoossen@gmail.com>
2026-08-19 12:24:54 +0000

    Create Culto ICE Nova Russas “o-arrebatamento-da-igreja_1-tessalonicenses-4-13-18-pr-humberto-duarte”

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //Somehow I need to get a build tag built into thise serviceworker file, because caches will only be updated if this file changes.
 
-const buildTag = "2026818173724";
-const precachedResources = ["/", "/algemas-quebradas/", "/recursos/capas/semmarca/algemas-quebradas.webp", "/culto-ice-nova-russas/", "/recursos/capas/semmarca/culto-ice-nova-russas.webp", "/familia-bencao-de-deus/", "/recursos/capas/semmarca/familia-bencao-de-deus.webp", "/jornal-seara/", "/recursos/capas/semmarca/jornal-seara.webp", "/ligado-na-verdade/", "/recursos/capas/semmarca/ligado-na-verdade.webp", "/minha-historia/", "/recursos/capas/semmarca/minha-historia.webp", "/seara-esporte-clube/", "/recursos/capas/semmarca/seara-esporte-clube.webp", "/a-esposa-por-tras/", "/parceiros/", "/baixar-algemas-quebradas/", "/baixar-minha-historia/", "/styles.css"];
+const buildTag = "202681912248";
+const precachedResources = ["/", "/algemas-quebradas/", "/recursos/capas/semmarca/algemas-quebradas.webp", "/culto-ice-nova-russas/", "/recursos/capas/semmarca/culto-ice-nova-russas.webp", "/familia-bencao-de-deus/", "/recursos/capas/semmarca/familia-bencao-de-deus.webp", "/jornal-seara/", "/recursos/capas/semmarca/jornal-seara.webp", "/ligado-na-verdade/", "/recursos/capas/semmarca/ligado-na-verdade.webp", "/minha-historia/", "/recursos/capas/semmarca/minha-historia.webp", "/seara-esporte-clube/", "/recursos/capas/semmarca/seara-esporte-clube.webp", "/baixar-algemas-quebradas/", "/parceiros/", "/a-esposa-por-tras/", "/baixar-minha-historia/", "/styles.css"];
 const origin = new URL(location).origin;
 
 // Live streams should never be cached - they're real-time and constantly changing
